723661a7ee540b06d7464a8d15a3b940c0508c4f
TaufiqDatau <taufiqhdatau@gmail.com>
2024-10-12 00:36:20 +0700

    adding lexer so it can read function and identifier

--- token/token.go
+++ token/token.go
@@ -32,3 +32,15 @@ const(
   FUNCTION = "FUNCTION"
   LET = "LET"
 )
+var keywords = map[string]TokenType{
+  "fn": FUNCTION,
+  "let": LET,
+}
+
+func LookupIndent(indent string) TokenType{
+  if tok, ok := keywords[indent]; ok{
+    return tok
+  }
+
+  return IDENT
+}
